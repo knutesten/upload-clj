8b9f310ee5b96a5c444190f5dd15eede5f879f40
Knut Esten Melandsø Nekså <knutn@netcompany.com>
2021-06-20 23:11:24 +0200

    Adding nonce and PKCE

--- src/no/neksa/upload/keycloak.clj
+++ src/no/neksa/upload/keycloak.clj
@@ -1,6 +1,7 @@
 (ns no.neksa.upload.keycloak
   (:require
    [clojure.edn :as edn]
+   [clojure.string :as str]
    [clj-http.client :as client]
    [ring.util.response :refer [redirect response]]
    [cheshire.core :as json]))
@@ -22,20 +23,24 @@
 
 (defn oauth-authorization-uri
   "Creates an authorization uri"
-  [req state]
+  [req state nonce code-challenge]
   (str (:authorization_endpoint config)
        "?state=" state
+       "&nonce=" nonce
        "&redirect_uri=" (callback-url req)
        "&client_id=" (:client-id config)
+       "&code_challenge=" code-challenge
+       "&code_challenge_method=S256"
        "&response_type=code"
        "&scope=openid profile email"))
 
-(defn fetch-access-token [req code]
+(defn fetch-access-token [req code code-verifier]
   (-> (client/post (:token_endpoint config)
                    {:accept        :json
                     :as            :json
                     :cookie-policy :standard
                     :form-params   {:grant_type    "authorization_code"
+                                    :code_verifier code-verifier
                                     :client_id     (:client-id config)
                                     :client_secret (:client-secret config)
                                     :code          code
@@ -51,35 +56,53 @@
     (String. %)
     (json/parse-string % true)))
 
-(defn code->claims [req code]
-  (->> code
-       (fetch-access-token req)
+(defn code->claims [req code code-verifier]
+  (->> (fetch-access-token req code code-verifier)
        access-token->claims))
 
 (defn authentication-handler [req]
   (let [code          (-> req :query-params (get "code"))
         state         (-> req :query-params (get "state"))
-        session-state (-> req :session :state)]
-    (if (= state session-state)
+        session-state (-> req :session :state)
+        nonce         (-> req :session :nonce)
+        code-verifier (-> req :session :code-verifier)
+        claims        (code->claims req code code-verifier)]
+    (if (and (= state session-state)
+             (= nonce (:nonce claims)))
       (-> (redirect "/")
           (assoc :session (with-meta
-                            {:identity (code->claims req code)}
+                            {:identity claims}
                             {:recreate true})))
-      (-> (response (str "Invalid state " state " " session-state))
+      (-> (response (str "Invalid state " state " " session-state " or\n"
+                         "invalid nonce " nonce " " (:nonce claims)))
           (assoc :status 401)))))
 
 (defn random-string [len]
-  (let [sample        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  (let [sample        "-._~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         sample-size   (count sample)
         secure-random (java.security.SecureRandom.)]
     (->> (repeatedly #(nth sample (.nextInt secure-random sample-size)))
          (take len)
          (apply str))))
 
+(defn code-verifier->code-challenge [code-verifier]
+  (let [encoder (java.util.Base64/getUrlEncoder)
+        s256    (java.security.MessageDigest/getInstance "SHA-256")
+        encoded (->> code-verifier
+                     (.getBytes)
+                     (.digest s256)
+                     (.encodeToString encoder))]
+    (str/replace encoded #"^(.*)=" "$1")))
+
 (defn login-handler [req]
-  (let [state (random-string 40)]
-    (-> (redirect (oauth-authorization-uri req state))
-        (assoc-in [:session :state] state))))
+  (let [state          (random-string 40)
+        nonce          (random-string 40)
+        code-verifier  (random-string 100)
+        code-challenge (code-verifier->code-challenge code-verifier)]
+    (-> (redirect (oauth-authorization-uri req state nonce code-challenge))
+        (assoc-in [:session :state] state)
+        (assoc-in [:session :nonce] nonce)
+        (assoc-in [:session :code-verifier] code-verifier))))
 
 (defn logout-handler [req]
   (-> (redirect (str (:end_session_endpoint config)
